450303cf42d273f6dce93d4ade8c583c1df0c70a
hoangtran98 <105474302+hoangtran98@users.noreply.github.com>
2022-09-12 18:34:33 +0100

    'readme'

---
+++ README.md
@@ -0,0 +1,11 @@
+<h2>Simple Web Scraper using BeautifulSoup Python</h2>
+
+<h3>Installing dependencies</h3>
+pip install requests beautifulsoup
+
+<h3>Make it your own?</h3>
+<ul>
+  <li>Step 1: change the target URL</li>
+  <li>Step 2: modify the content tags</li>
+  <li>Step 3: modify the csv output headers</li>
+</ul>
--- scraper.py
+++ scraper.py
@@ -2,33 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+# target url for scraping
 URL = "http://www.values.com/inspirational-quotes"
+
+# create a response object
 r = requests.get(URL)
 
+# parse the content of the response
 soup = BeautifulSoup(r.content, 'html5lib')
 
 quotes = []
 
+# extract all divs with specific id
 table = soup.find('div', attrs={'id': 'all_quotes'})
 
-print(table)
-
 for row in table.findAll(
-    'div',
-    attrs = {'class':'col-6 col-lg-4 text-center margin-30px-bottom sm-margin-30px-top'}):
+        'div',
+        attrs={
+            'class':
+            'col-6 col-lg-4 text-center margin-30px-bottom sm-margin-30px-top'
+        }):
 
-  quote = {}
-  quote['theme'] = row.h5.text
-  quote['url'] = row.a['href']
-  quote['img'] = row.img['src']
-  quote['lines'] = row.img['alt'].split(" #")[0]
-  quote['author'] = row.img['alt'].split(" #")[1]
-  quotes.append(quote)
+    # build a quote dict
+    quote = {}
+    quote['theme'] = row.h5.text
+    quote['url'] = row.a['href']
+    quote['img'] = row.img['src']
+    quote['lines'] = row.img['alt'].split(" #")[0]
+    quote['author'] = row.img['alt'].split(" #")[1]
 
+    # Add a quote to the list
+    quotes.append(quote)
+
+# saving filename
 filename = 'inspirational_quotes.csv'
 
+# write all quotes to the csv file
 with open(filename, 'w', newline='') as f:
-  w = csv.DictWriter(f, ['theme', 'url', 'img', 'lines', 'author'])
-  w.writeheader()
-  for quote in quotes:
-    w.writerow(quote)
+    w = csv.DictWriter(f, ['theme', 'url', 'img', 'lines', 'author'])
+    w.writeheader()
+
+    for quote in quotes:
+        w.writerow(quote)
